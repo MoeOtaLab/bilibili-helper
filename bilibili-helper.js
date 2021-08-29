2a79fe0e2803909e3c3175dda4cf7766b90a6920
everbrez <everbrez@gmail.com>
2021-08-29 23:14:10 +0800

    add control panel

--- bilibili-helper.js
+++ bilibili-helper.js
@@ -1,6 +1,6 @@
 "use strict";
 // ==UserScript==
-// @name                bilibili Helper
+// @name                bilibili Helper2
 // @name:zh-CN          bilibili 助手
 // @description         Auto disable bilibili HTML5 player danmaku. Auto widescreen. Add hotkeys（d: toggle danmaku, c: give coins, s: add collections）.
 // @description:zh-CN   自动关闭哔哩哔哩 HTML5 播放器弹幕，自动宽屏，添加快捷键（d:弹幕切换，c：投币，s：收藏）.
@@ -8,6 +8,10 @@
 // @version             2021.08.29
 // @author              everbrez
 // @license             MIT License
+// @grant               unsafeWindow
+// @grant               GM_registerMenuCommand
+// @grant               GM_setValue
+// @grant               GM_getValue
 // @match               *://www.bilibili.com/video/*
 // @match               *://www.bilibili.com/bangumi/play/*
 // @match               *://www.bilibili.com/blackboard/*
@@ -64,7 +68,6 @@ function waitForElement(targetSelector, rootSelector = 'body', wait) {
 }
 
 async function autoClickElement(targetSelector, rootSelector, now = false) {
-  console.log('set==> ', targetSelector)
   if (now) {
     const parent = rootSelector ? document.querySelector(rootSelector) : document;
     if (parent) {
@@ -77,7 +80,6 @@ async function autoClickElement(targetSelector, rootSelector, now = false) {
     return false;
   }
   const target = await waitForElement(targetSelector, rootSelector);
-  console.log('auto click:', target)
   target.click();
 }
 
@@ -92,15 +94,18 @@ function detectIsInputing() {
 }
 
 function getStorageConfig(key) {
+  let data = null
   try {
-    const data = JSON.parse(key)
-    if (data instanceof Object) {
-      return data
+    const rawData = localStorage.getItem(key)
+    const parseData = JSON.parse(rawData)
+    if (parseData instanceof Object) {
+      data = parseData;
     }
   } catch (error) {
     // ignore..
+    console.error(error)
   } finally {
-    return null
+    return data
   }
 }
 
@@ -108,6 +113,162 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 /** END: utils */
+
+/** panel element */
+class ConfigPanel extends HTMLElement {
+  static get observedAttributes() {
+    return ['open', 'value'];
+  }
+
+  constructor() {
+    super()
+    this._value = {}
+    this.ready = false
+    this.attachShadow({
+      mode: 'open'
+    })
+
+    const style = document.createElement('style')
+    style.innerHTML = `
+      .dialog {
+        position: fixed;
+        right: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 9999;
+    
+        display: grid;
+        align-items: center;
+        justify-content: end;
+
+        padding: 16px;
+
+        background: white;
+        border-radius: 4px;
+        overflow: hidden;
+
+        font-size: 14px;
+      }
+
+      .dialog:not(.show) {
+        display: none;
+      }
+
+      .panel {
+        display: grid;
+        grid-auto-flow: row;
+        gap: 8px;
+      }
+    
+      .panel>div {
+        display: grid;
+        grid-auto-flow: column;
+        justify-content: start;
+        gap: 8px;
+      }
+    
+      .panel select {
+        border: none;
+        cursor: pointer;
+      }
+    `
+
+    const div = document.createElement('div')
+    div.innerHTML = `
+      <div class="dialog">
+        <form class="panel">
+          <div>
+            <label for="defaultDanmakuStatus"> 默认弹幕状态 </label>
+            <select id="defaultDanmakuStatus">
+              <option value="on">开启</option>
+              <option value="off">关闭</option>
+              <option value="default">默认</option>
+            </select>
+          </div>
+        
+          <div>
+            <label for="defaultScreenStatus"> 默认屏幕状态 </label>
+            <select id="defaultScreenStatus">
+              <option value="widescreen">宽屏</option>
+              <option value="default">默认</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    `
+
+    this.shadowRoot.append(style, div)
+    this.handleClickOutside = this.handleClickOutside.bind(this)
+    this.closeDialog = this.closeDialog.bind(this)
+    this.openDialog = this.openDialog.bind(this)
+  }
+
+  connectedCallback() {
+    document.addEventListener('click', this.handleClickOutside, false)
+  }
+
+  disconnectedCallback() {
+    document.removeEventListener('click', this.handleClickOutside, false)
+  }
+
+  handleClickOutside(event) {
+    if (event.target !== this) {
+      this.closeDialog();
+    }
+  }
+
+  get form() {
+    return this.shadowRoot.querySelector('form')
+  }
+
+
+  closeDialog() {
+    const dialog = this.shadowRoot.querySelector('.dialog');
+    dialog && dialog.classList.remove('show');
+    if (this.ready) {
+      const value = Object.fromEntries([...this.form.elements].map(el => [el.id, el.value]))
+      this.value = JSON.stringify(value)
+      this.dispatchEvent(new Event('change'))
+    }
+  }
+
+  openDialog() {
+    const dialog = this.shadowRoot.querySelector('.dialog')
+    dialog && dialog.classList.add('show')
+  }
+
+  resetFormValues() {
+    [...this.form.elements].forEach((el) => {
+      el.value = this._value[el.id];
+    })
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.ready = true;
+    if (name === 'open') {
+      if (newValue) {
+        this.openDialog();
+      } else {
+        this.closeDialog();
+      }
+    }
+
+    if (name === 'value') {
+      try {
+        const currentValue = JSON.parse(newValue);
+        // this. = currentValue;
+        if (currentValue instanceof Object) {
+          this._value = currentValue
+          this.resetFormValues();
+        }
+      } catch (error) {
+        // ignore
+      }
+    }
+  }
+}
+
+/** end panel element */
 (function () {
   /** global config */
   const DEFAULT_CONFIG_KEY = 'BILIBILI_HELPER_CONFIG'
@@ -138,14 +299,17 @@ function sleep(ms) {
     }],
   }
 
-  const _config = getStorageConfig(DEFAULT_CONFIG_KEY) || defaultConfig;
+  const _config = Object.assign({
+    ...defaultConfig
+  }, getStorageConfig(DEFAULT_CONFIG_KEY) || {});
 
   const config = new Proxy(_config, {
     set(target, prop, value, receiver) {
-      localStorage.setItem(DEFAULT_CONFIG_KEY, _config)
-      return Reflect.set(target, prop, value, receiver)
-    }
-  })
+      const res = Reflect.set(target, prop, value, receiver)
+      localStorage.setItem(DEFAULT_CONFIG_KEY, JSON.stringify(_config, undefined, 2))
+      return res;
+    },
+  });
 
   async function autoClickElementAfterFound(targetSelector, foundSelector) {
     await waitForElement(foundSelector)
@@ -158,13 +322,11 @@ function sleep(ms) {
   function setDefaultDanmakuStatus() {
     switch (config.defaultDanmakuStatus) {
       case 'on':
-        console.log('===on===')
         return autoClickElementAfterFound('input.bui-switch-input:not(:checked)', 'input.bui-switch-input')
       case 'default':
         return;
       case 'off':
       default:
-        console.log('===off===', document.querySelector('input.bui-switch-input:checked'))
         return autoClickElementAfterFound('input.bui-switch-input:checked', 'input.bui-switch-input')
     }
   }
@@ -203,10 +365,37 @@ function sleep(ms) {
     });
   }
 
+  /** create panel */
+  function createPanel() {
+    unsafeWindow.customElements.define('bilibili-helper-panel', ConfigPanel);
+    const panel = document.createElement('bilibili-helper-panel');
+    GM_registerMenuCommand('打开配置', (...args) => {
+      const currentStatus = panel.getAttribute('open');
+      if (currentStatus) {
+        panel.setAttribute('open', '');
+      } else {
+        panel.setAttribute('open', 'true');
+        panel.setAttribute('value', JSON.stringify(config));
+      }
+    }, 'openConfig')
+
+    panel.addEventListener('change', (event) => {
+      try {
+        const value = JSON.parse(event.target.value);
+        Object.assign(config, value);
+      } catch (error) {
+        // ignore
+      }
+    })
+
+    document.body.append(panel);
+  }
+
   function main() {
     setDefaultDanmakuStatus();
     setScreenStatus();
     registerHotKeys();
+    createPanel();
   }
 
   main();
